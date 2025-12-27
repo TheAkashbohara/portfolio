b96ed465a1a0e5d296d1b1bdb31e95c3ad9aa009
theakashbohara <spfystudio@gmail.com>
2025-12-27 22:22:03 +0545

    git f

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -22,7 +22,7 @@ export default function ContactSection() {
                     </div>
 
                     {/* Small "HIRE ME" Bubble */}
-                    <Link href="https://mail.google.com/mail/?view=cm&fs=1&to=thisisartistakash@gmail.com" className="relative group cursor-pointer mr-[120px]">
+                    <Link target="_blank" href="https://mail.google.com/mail/?view=cm&fs=1&to=thisisartistakash@gmail.com" className="relative group cursor-pointer mr-[120px]">
                         <svg width="100" height="100" viewBox="0 0 100 100" className="drop-shadow-sm transition-transform group-hover:scale-105">
                             <circle cx="50" cy="50" r="48" fill="#e0c969" stroke="black" strokeWidth="2" />
                         </svg>
